f18574c06f22581df6a4d96945726d0bbddd6107
Prayer <cpf2015@gmail.com>
2017-07-16 15:57:01 +0800

    0.1

---
+++ source/js/main.js
@@ -0,0 +1,10 @@
+(function() {
+  // Caption
+  $('.article-entry').each(function(i) {
+    $(this).find('img').each(function() {
+      if (this.alt) {
+        $(this).after('<span class="caption">' + this.alt + '</span>');
+      }
+    });
+  });
+})()
