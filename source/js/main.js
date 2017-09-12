dac416debf6d3360b769e51b7c3cbc5bd8fba82b
prayer <cpf2015@163.com>
2017-09-12 10:03:52 +0800

    update

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,9 @@
-(function() {
-  // Caption
-  $('.article-entry').each(function(i) {
-    $(this).find('img').each(function() {
-      if (this.alt) {
-        $(this).after('<span class="caption">' + this.alt + '</span>');
-      }
-    });
-  });
-})()
+$(function(){
+	$('.article-entry').each(function(i) {
+	  $(this).find('img').each(function() {
+	    if (this.alt) {
+	      $(this).after('<span class="caption">' + this.alt + '</span>');
+	    }
+	  })
+	})
+})
