c944f35e553be758f732cb146934beddbf205376
prayer <cpf2015@163.com>
2017-12-22 16:31:00 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ### hexo-theme-neat
 
-随意写了一个主题，很简陋，很多功能都没有:P，[demo](http://www.shinji.me)
+随意写了一个主题，很简陋，很多功能都没有:P，[demo](http://blog.shinji.me)
 
 看心情跟时间偶尔更新，不断更新设计和功能
 
@@ -12,8 +12,6 @@ _几个注意点_
 
 ### 评论系统
 仅支持[hypercomments](https://www.hypercomments.com/)（因为其他的都太丑了）,主题配置文件里添加你的 ID 即可，ID在你进入 admin 页面，点击右上角齿轮图标，新创建 widget 后即可在下图标注位置中看到
-
-![](http://7xpqse.com1.z0.glb.clouddn.com/theme_config_md.png)
 
 ### tags页面与category页面
 
@@ -32,6 +30,6 @@ layout: tags.ejs
 # categories
 layout: categories.ejs
 
-# 如果需要关闭评论
+# 如果某篇需要关闭评论，markdown里设置Front-matter
 comments: flase
 ```
--- source/js/main.js
+++ source/js/main.js
@@ -1,12 +1,11 @@
-$(function(){
-	$('.article-entry').each(function(i) {
-	  $(this).find('img').each(function() {
-	    if (this.alt) {
-	      $(this).after('<span class="caption">' + this.alt + '</span>');
-	    }
-	  })
-	})
-	$('.navbar').click(function(){
-		$('#container').toggleClass('sidenavopen')
-	})
-})
+(function() {
+    var arc = document.querySelector('.article-entry'),
+        arcimg = arc.querySelectorAll('img')
+    arcimg.forEach(ele => {
+        if (ele.getAttribute("alt")) {
+            ele.insertAdjacentHTML('afterEnd', '<span class="caption">' + ele.getAttribute("alt") + '</span>')
+        }
+    });
+    var symbols = '<svg style="display:none; width:0; height:0; visibility:hidden;position:absolute;z-index:-1"><symbol id="icon-tag" viewBox="0 0 512 512"><title>tag</title><g><g class="icon-wrapper" fill="currentColor"><path d="M464 32H304L48 320l160 160 256-288V32zm-16 152L208.125 456 72.062 320 311.587 48H448v136z" /><path d="M368 160c17.645 0 32-14.355 32-32s-14.355-32-32-32-32 14.355-32 32 14.355 32 32 32zm0-48c8.836 0 16 7.163 16 16s-7.164 16-16 16-16-7.163-16-16 7.164-16 16-16z" /></g></g></symbol><symbol id="heart" viewBox="0 0 512 512"><title>heart</title><g><g class="nc-icon-wrapper" fill="currentColor"><path d="M429.9 95.6c-40.4-42.1-106-42.1-146.4 0L256 124.1l-27.5-28.6c-40.5-42.1-106-42.1-146.4 0-45.5 47.3-45.5 124.1 0 171.4L256 448l173.9-181c45.5-47.3 45.5-124.1 0-171.4z"/></g></g></symbol></svg>'
+    document.body.insertAdjacentHTML("afterBegin", symbols)
+})()
